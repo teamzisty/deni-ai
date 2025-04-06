0ded144a21b9d41905c1c5b19b6f2e6cffce0cac
raicdev <admin@raic.dev>
2025-04-06 20:29:13 +0900

    fix(www): Enable React Scan only Development

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "next-themes";
 import { Noto_Sans_JP } from "next/font/google";
 import { JetBrains_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const notoSansJP = Noto_Sans_JP({
   subsets: ["latin"],
@@ -54,10 +55,14 @@ export default function RootLayout({
             `,
           }}
         />
-        <script
-          crossOrigin="anonymous"
-          src="//unpkg.com/react-scan/dist/auto.global.js"
-        />
+        
+        {process.env.NODE_ENV === 'development' && (
+          <Script
+            strategy="afterInteractive"
+            crossOrigin="anonymous"
+            src="//unpkg.com/react-scan/dist/auto.global.js"
+          />
+        )}
       </head>
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} ${notoSansJP.variable} bg-background font-sans antialiased text-foreground`}
