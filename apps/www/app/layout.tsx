2d2cd67e324198fd1e78119e779fc9d3197e0207
raicdev <admin@raic.dev>
2025-05-06 14:45:15 +0900

    feat(www): Add Twitter tracking script to RootLayout component

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -36,6 +36,26 @@ export default async function RootLayout({
             src="//unpkg.com/react-scan/dist/auto.global.js"
           />
         )}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                !function(e,t,n,s,u,a)
+                {e.twq ||
+                  ((s = e.twq =
+                    function () {
+                      s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+                    }),
+                  (s.version = "1.1"),
+                  (s.queue = []),
+                  (u = t.createElement(n)),
+                  (u.async = !0),
+                  (u.src = "https://static.ads-twitter.com/uwt.js"),
+                  (a = t.getElementsByTagName(n)[0]),
+                  a.parentNode.insertBefore(u, a))}
+                (window,document,'script'); twq('config','potlg');
+              `,
+          }}
+        />
       </head>
       <body
         className={`${inter.variable} ${jetbrainsMono.variable} ${notoSansJP.variable} bg-background font-sans antialiased text-foreground`}
@@ -54,24 +74,12 @@ export default async function RootLayout({
           }}
         >
           {children}
+
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                !function(e,t,n,s,u,a)
-                {e.twq ||
-                  ((s = e.twq =
-                    function () {
-                      s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
-                    }),
-                  (s.version = "1.1"),
-                  (s.queue = []),
-                  (u = t.createElement(n)),
-                  (u.async = !0),
-                  (u.src = "https://static.ads-twitter.com/uwt.js"),
-                  (a = t.getElementsByTagName(n)[0]),
-                  a.parentNode.insertBefore(u, a))}
-                (window,document,'script'); twq('config','potlg');
-              `
+              twq('event', 'tw-potlg-potli', {});
+            `,
             }}
           />
         </ThemeProvider>
