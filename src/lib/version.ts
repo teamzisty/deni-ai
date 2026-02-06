abd5dface1161d034b3a9a115d6f9e6ab3b44c52
rai <imraicdev@gmail.com>
2026-02-06 23:45:53 +0900

    fix: update Japanese messages and bump to v6.4

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,6 +1,6 @@
-export const appVersion = "6.4-beta.3";
+export const appVersion = "6.4";
 export const appCodename = "Crimson Titan";
-export const appDate = "2026-02-02";
+export const appDate = "2026-02-06";
 
 const appHashPayload = [appVersion, appDate].join(":");
 export const appHash = globalThis.btoa(appHashPayload);
