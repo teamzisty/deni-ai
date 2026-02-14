79e3973e1d4719469912c6f59d7922c275098f1d
rai <imraicdev@gmail.com>
2026-02-14 23:47:23 +0900

    chore: remove unused middleware file and related imports

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { NextRequest } from "next/server";
-import { proxy, config as proxyConfig } from "@/proxy";
-
-export function middleware(request: NextRequest) {
-  return proxy(request);
-}
-
-export const config = proxyConfig;
