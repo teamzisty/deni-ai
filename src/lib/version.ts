793d028293f4b060b56e5190a3bb0e4acbe96a75
rai <imraicdev@gmail.com>
2026-03-13 13:49:25 +0900

    chore: Bump app version to 7.0-beta.3 [skip ci]

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,6 +1,6 @@
-export const appVersion = "7.0-beta.2";
+export const appVersion = "7.0-beta.3";
 export const appCodename = "Phantom Phoenix";
-export const appDate = "2026-3-12";
+export const appDate = "2026-3-13";
 
 const appHashPayload = [appVersion, appDate].join(":");
 export const appHash = globalThis.btoa(appHashPayload);
