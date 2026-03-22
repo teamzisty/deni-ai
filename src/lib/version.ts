e7512274db4d60c3fe941b11e70810ede1e743bc
rai <imraicdev@gmail.com>
2026-03-22 13:31:36 +0900

    chore: bump to v7.2 [skip ci]

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,6 +1,6 @@
-export const appVersion = "7.1.1";
-export const appCodename = "Solar Sphinx";
-export const appDate = "2026-03-21";
+export const appVersion = "7.2";
+export const appCodename = "Iron Wolf";
+export const appDate = "2026-03-22";
 
 const appHashPayload = [appVersion, appDate].join(":");
 export const appHash = globalThis.btoa(appHashPayload);
