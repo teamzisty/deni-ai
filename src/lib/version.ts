f64274965d55f9da06d9f45cd87aed70fa563ebd
rai <imraicdev@gmail.com>
2026-07-10 18:52:30 +0900

    chore: bump version to 7.5.0 (Crystal Tiger)

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,6 +1,6 @@
-export const appVersion = "7.4.1";
-export const appCodename = "Lunar Hawk";
-export const appDate = "2026-07-05";
+export const appVersion = "7.5.0";
+export const appCodename = "Crystal Tiger";
+export const appDate = "2026-07-10";
 
 const appHashPayload = [appVersion, appDate].join(":");
 export const appHash = globalThis.btoa(appHashPayload);
